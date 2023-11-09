0e2bec59a48af3dd1fa49e8d4e25df6eb0954b5a
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2023-11-09 12:32:52 +0100

    Setup basic project

---
+++ src/test/java/ch/heig/MyTest.java
@@ -0,0 +1,14 @@
+package ch.heig;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MyTest {
+	@Test
+	void trueIsTrue() {
+		assertTrue(true);
+	}
+}
