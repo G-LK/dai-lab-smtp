07c1640f757ca944ee57fb0eeeeeff38af8a4a26
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2023-11-18 17:33:09 +0100

    Add getters and prepare() on Sender, change constructor

--- src/main/java/ch/heig/Sender.java
+++ src/main/java/ch/heig/Sender.java
@@ -3,20 +3,41 @@ package ch.heig;
 public class Sender {
 	final int groupsNB;
 	Email[] emails;
+	String messagesFilePath;
+	String victimsFilePath;
+	String[] victimsList;
 
 	// TODO
-	public Sender(int groupsNB) {
+	public Sender(int groupsNB, String messagesFilePath, String victimsFilePath) {
 		this.groupsNB = groupsNB;
+		this.messagesFilePath = messagesFilePath;
+		this.victimsFilePath = victimsFilePath;
+	}
+
+	public void prepare() {
+		// TODO extractions
+		generateEmails();
+	}
+
+	public String[] getVictimsList() {
+		return victimsList;
+	}
+
+	public String[] getMessages() {
+		return victimsList;
 	}
 
 	// TODO:
 	public void send() {
-		generateEmails();
 		sendEmails();
 	}
 
-	public void generateEmails() {
+	void generateEmails() {
 
+	}
+
+	public Email[] getEmails() {
+		return emails;
 	}
 
 	public void sendEmails() {
