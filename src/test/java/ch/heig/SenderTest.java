e6b470ae785ed6a341911684763df0788199ef86
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2023-12-01 00:24:45 +0100

    Fix SenderTest

--- src/test/java/ch/heig/SenderTest.java
+++ src/test/java/ch/heig/SenderTest.java
@@ -2,22 +2,44 @@ package ch.heig;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.HashSet;
 
 public class SenderTest {
 	@Test
 	void generateEmailsWorks() {
 
-		Sender s = new Sender();
-		assertEquals(new Email[] {}, s.getEmails());
+		Sender s = new Sender(5);
+		//
+		assertEquals(0, s.getEmails().size());
+
 		s.prepare();
-		s.generateEmails();
-		Email[] emails = s.getEmails();
-		for (var e : emails) {
-			assertTrue(e.getTo().length >= 1);
-			assertTrue(e.getTo().length <= 4);
+		// Run 100 times the email generation to make sure we can catch errors
+		// even with random in play
+		for (int i = 0; i < 100; i++) {
+			s.generateEmails();
+			var emails = s.getEmails();
+			assertTrue(s.getEmails().size() > 0);
+			HashSet<String> toList = new HashSet<>();
+			for (var e : emails) {
+				// Make sure all to adresses are unique
+				for (var to : e.getTo()) {
+					toList.add(to);
+				}
+				assertEquals(e.getTo().length, toList.size());
+
+				// Assert from address is not part of the to adresses:
+				assertFalse(toList.contains(e.getFrom()));
+
+				// Check number of groups
+				assertTrue(e.getTo().length >= 1); // at minimum one receiver
+				assertTrue(e.getTo().length <= 4); // 5 at max minus the sender
+			}
 		}
 
 		// Add more tests related to README instructions...
@@ -25,10 +47,9 @@ public class SenderTest {
 
 	@Test
 	void configCanBeLoaded() {
-		Sender s = new Sender();
+		Sender s = new Sender(5);
 		s.loadConfig();
-		assertEquals(5, s.config.groupNumber);
-		assertEquals(
+		assertArrayEquals(
 				new String[] { "hey@example.com",
 						"hello@example.com",
 						"john@example.com",
@@ -38,10 +59,9 @@ public class SenderTest {
 				s.config.addresses);
 
 		// Make sure the first message is loaded (others are probably too)
-		var e = new FakeMessage();
-		e.subject = "Hey guys";
-		e.body = "you won 100000$\nRegards\nThe US president.";
-		assertEquals(e, s.config.messages[0]);
+
+		assertEquals("Hey guys", s.config.messages[0].subject);
+		assertEquals("you won 100000$\nRegards\nThe US president.", s.config.messages[0].body);
 		assertEquals(4, s.config.messages.length);
 	}
 }
