71b826d8d3c999ab1a4e71b25140c9765d150db0
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2023-12-01 01:20:23 +0100

    Restructure config, implement parsing, test parsed values

--- src/main/java/ch/heig/Config.java
+++ src/main/java/ch/heig/Config.java
@@ -1,13 +1,61 @@
 package ch.heig;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import com.google.gson.Gson;
 
 public class Config {
-
-	int groupNumber;
-	String[] addresses;
+	String[] victims;
 	FakeMessage[] messages;
 
+	public Config() {
+		load(readFile(Main.VICTIMS_FILE), readFile(Main.MESSAGES_FILE));
+	}
+
+	// For easy testing
+	Config(String victimsJson, String messagesJson) {
+		load(victimsJson, messagesJson);
+	}
+
+	private void load(String victimsJson, String messagesJson) {
+		if (victimsJson == null || victimsJson.isEmpty() || messagesJson == null || messagesJson.isEmpty())
+			throw new RuntimeException();
+
+		// Parse content with the help of Gjson library
+		Gson gson = new Gson();
+		System.out.println("victimsJson: " + victimsJson);
+		victims = gson.fromJson(victimsJson, String[].class);
+		System.out.println("messagesJson: " + messagesJson);
+		messages = gson.fromJson(messagesJson, FakeMessage[].class);
+	}
+
+	// Read a given file entirely
+	private String readFile(String path) {
+		String content = "";
+		try (BufferedReader in = new BufferedReader(
+				new InputStreamReader(
+						new FileInputStream(path),
+						StandardCharsets.UTF_8))) {
+			String line;
+			while ((line = in.readLine()) != null) {
+				content += line + '\n';
+			}
+		} catch (Exception e) {
+			System.out.println("Error in reading file '" + path + "': " + e);
+			return null;
+		}
+		return content;
+	}
+
+	// Validate the extracted victims and messages and return an array of errors
+	// in case they exists
 	public String[] validate() {
+		// TODO: do the checks (see more in tests)
 		return new String[] {};
 	}
 }
--- src/test/java/ch/heig/ConfigTest.java
+++ src/test/java/ch/heig/ConfigTest.java
@@ -2,31 +2,63 @@ package ch.heig;
 
 import org.junit.jupiter.api.Test;
 
+import com.google.gson.JsonSyntaxException;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ConfigTest {
+	final static String from = "alice@localhost.com";
+	final static String[] to = { "john@localhost.com", "bob@localhost.com" };
 
 	@Test
-	void parseWithInvalidJsonOrInvalidEmailsFails() {
-		// TODO: should we change the exception class ??
-		// Invalid JSON
-		assertThrows(RuntimeException.class, () -> {
-			new Email("{blab invalid \"", from, to);
-		});
-		// Missing content
-		assertThrows(RuntimeException.class, () -> {
-			new Email("{\"subject\": \"\", \"body\":\"hello world\"}", from, to);
-		});
-		// Missing body field
-		assertThrows(RuntimeException.class, () -> {
-			new Email("{\"subject\": \"hello there\"}", from, to);
-		});
+	void loadConfigOnGivenJsonWorks() {
+		var c = new Config("[\"hey@hey.com\",\"asdf@wwwww.com\",\"fff@ddd.com\"]",
+				"[{\"subject\": \"hello there\", \"body\": \"hello body\"}," +
+						"{\"subject\": \"hi\", \"body\": \"nice\\nto\\nmeet you\"}]");
 
-		// Missing subject field
+		assertEquals(3, c.victims.length);
+		assertEquals("hey@hey.com", c.victims[0]);
+
+		assertEquals("hello there", c.messages[0].subject);
+		assertEquals("hello body", c.messages[0].body);
+		assertEquals(2, c.messages.length);
+	}
+
+	@Test
+	void loadConfigManageErrors() {
+		// TODO: should we change the exception class ??
+		// Empty files
 		assertThrows(RuntimeException.class, () -> {
-			new Email("{\"body\": \"hello there\"}", from, to);
+			new Config("", null);
 		});
+		// Invalid JSON
+		assertThrows(JsonSyntaxException.class, () -> {
+			new Config("{blab invalid \"", "aajajajaja");
+		});
+	}
+
+	@Test
+	void validationWorks() {
+		var c = new Config("\"example.com\",\"a@com\",\"john@example.com\",",
+				"[" +
+						"{\"subject\": \"hello there\"}, " +
+						"{\"body\": \"hello there\"}, " +
+						"{\"unkown\": true}, " +
+						"{\"subject\": \"hello there\",\"body\": \"\"}," +
+						"{\"subject\": \"hello there\",\"body\": \"\"}" +
+						"]");
+		assertArrayEquals(new String[] {
+				"Victim 1 is not a valid email",
+				"Victim 2 is not a valid email",
+				"Email 1 has no body",
+				"Email 2 has no body",
+				"Email 3 has no subject",
+				"Email 3 has no body",
+				"Email 4 has an empty body",
+				"Email 5 has an empty subject",
+		}, c.validate());
 	}
 }
